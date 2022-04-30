5095552ea108093bf57418dd46b2a0309c92f470
JonathanPegaz <pegazjonathan@gmail.com>
2022-04-30 18:27:25 +0200

    init

---
+++ backend/data/products.js
@@ -0,0 +1,58 @@
+const products = [
+    {
+      name: "Royal Damas",
+      imageUrl:
+        "/images/08_foam.png",
+      description:
+        "La Royal Damas, produit de luxe de la gamme, à l’amertume modérée généreusement houblonnée pour une explosion aromatique de fruits exotiqus et d’agrumes. Visuellement, c’est une bière très limpide avec une robe dorée et lumineuse qui attire irrésistiblement l’œil. Titre à 5,6%.",
+      price: 20,
+      countInStock: 1,
+    },
+    {
+      name: "Tim Crevard",
+      imageUrl:
+        "/images/02.png",
+      description:
+        "La plus abordable de la gamme. Cette bière blonde de fermentation basse à la couleur lumineuse et clair fera le plaisir des plus radins. Équilibrée et fine, elle titre 5% comme taux d'alcool. ",
+      price: 1,
+      countInStock: 100,
+    },
+    {
+      name: "Cpt Parrot",
+      imageUrl:
+        "/images/04_foam.png",
+      description:
+        "Le cpt Parrot cache bien son jeu. Sous ces airs de bière se cache un rhum. La composition est traversée par de subtiles notes de courge affinées par un bouquet de fines épices. Vous apprécierez la saveur automnale d'un rhum hors du commun.",
+      price: 6,
+      countInStock: 5,
+    },
+    {
+      name: "MoserFucker Maximator",
+      imageUrl:
+        "/images/10_foam.png",
+      description:
+        "Ne déclencher pas la colère d'une personne buvant cette bière. Titrant à 11,6° d'alcool, c'est la deuxième bière la plus puissante de la gamme. Légèrement pétillante, nous sommes sur une robe aux reflets orangés. En bouche, c'est une petite explosion de caramel et réglisse.",
+      price: 7,
+      countInStock: 25,
+    },
+    {
+      name: "Demont'toux",
+      imageUrl:
+        "/images/01_foam.png",
+      description:
+        "La plus intense de la gamme avec un taux d'alcool s'élevant à 12%. Elle est une bière blonde dotée d’un goût complet et puissant. Vous y reconnaitrez les saveurs de miel et de fruits sucrés, ce qui justifie son amertume unique. Soyez prêt à vous déchainer !",
+      price: 8,
+      countInStock: 4,
+    },
+    {
+      name: "Abbaye Mauche",
+      imageUrl:
+        "/images/06_foam.png",
+      description:
+        "Servie en girafe avec un taux à 8%. La Mauche présente une robe blonde-ambrée et une mousse crémeuse. Elle offre un nez très original, qui joue sur les notes boisées et épicées (girofle, vanille), mêlées à celles, fruitées de la pomme reinette bien mûre. Ces arômes se retrouvent en bouche dans un écrin de malt et une fine amertume.",
+      price: 12,
+      countInStock: 10,
+    },
+  ];
+  
+  module.exports = products;
